f1f9cea2fd1ff0ee211960bde44dfba7801628b5
Garrett <zerocool@Garretts-MacBook-Pro.local>
2017-12-16 09:59:13 -0600

    done

---
+++ vowelCount.js
@@ -0,0 +1,8 @@
+var vowelsCount = 0;
+ for(var i = 0; i < str.length; i ++){
+  if(str[i] === "a" || str[i] === "e" ||str[i] === "i" || str[i] === "o" || str[i] === "u"){
+    vowelsCount++;
+  }
+}
+return vowelsCount;
+}
