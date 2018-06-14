f782547d53174dd1e727287df57e0b8d9d5d4094
Garrett <garrett.chapman@yahoo.com>
2018-06-14 09:40:33 -0500

    containinationString alternate

--- contaminationString.js
+++ contaminationString.js
@@ -9,3 +9,14 @@ return text.replace(/./g, char);
 contamination("abc", "z") // "zzz"
 contamination("abc", " ") // "   "
 contamination("_3ebzgh4","&") // "&&&&&&&&"
+
+
+// ALTERNATE
+
+function contamination(text, char){
+  return char.repeat(text.length)
+}
+
+contamination("abc", "z") // "zzz"
+contamination("abc", " ") // "   "
+contamination("_3ebzgh4","&") // "&&&&&&&&"
