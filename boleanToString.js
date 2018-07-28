a3d8dc0101b2aae50e4eb974e705084aed3cee6b
Garrett <garrett.chapman@yahoo.com>
2018-07-28 18:10:35 -0500

    alternate solution

--- boleanToString.js
+++ boleanToString.js
@@ -1,3 +1,19 @@
 function booleanToString(b){
   return b ? 'true': 'false';
 }
+
+
+//ALTERNATE solutions
+
+
+// function booleanToString(b){
+//   return b.toString();
+// }
+
+// function booleanToString(b){
+// if (b === true) {
+//   return "true"
+// } else {
+//   return "false"
+//   }
+// }
