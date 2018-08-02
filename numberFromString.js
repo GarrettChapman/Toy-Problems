95d6f75d2aa015c14c8ad105b248aa8db14ea4a8
Garrett <garrett.chapman@yahoo.com>
2018-08-01 21:57:39 -0500

    alternate solution

--- numberFromString.js
+++ numberFromString.js
@@ -4,6 +4,14 @@ return Number(s.match(/\d/g).join(''));
 }
 
 
-
 getNumberFromString("123") //123
 getNumberFromString("this is number 7") // 7
+
+
+
+// ALTERNATE
+
+function getNumberFromString(s) {
+  x = Number(s.replace(/\D/g, ""));
+  return x;
+}
