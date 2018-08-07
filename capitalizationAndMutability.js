ec997c7555e006430146afb6877f4d25e0099a2b
Garrett <garrett.chapman@yahoo.com>
2018-08-06 22:58:01 -0500

    capitalization and Mutability

---
+++ capitalizationAndMutability.js
@@ -0,0 +1,15 @@
+function capitalizeWord(word) {
+
+
+var cap = word.replace(word[0], word[0].toUpperCase())
+
+return cap
+
+}
+
+
+
+
+capitalizeWord('word') // 'Word'
+capitalizeWord('i') // 'I'
+calizeWord('glasswear') // 'Glasswear'
