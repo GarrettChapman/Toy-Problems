f922b8b45bcd687c8e6c9c8791b1cc0fcaed88be
Garrett <garrett.chapman@yahoo.com>
2018-08-23 22:24:09 -0500

    update

--- vowelCount.js
+++ vowelCount.js
@@ -1,9 +1,15 @@
-var vowelsCount = 0;
- for(var i = 0; i < str.length; i ++){
-  if(str[i] === "a" || str[i] === "e" ||str[i] === "i" || str[i] === "o" || str[i] === "u"){
-    vowelsCount++;
+function vowelsCount (str)
+ var vowelsCount = 0;
+  for(var i = 0; i < str.length; i ++){
+    if(str[i] === "a" || str[i] === "e" ||str[i] === "i" || str[i] === "o" || str[i] === "u"){
+      vowelsCount++;
   }
 }
 return vowelsCount;
 }
 
+
+//Alternate solution
+// function vowelCount(str) {
+//   return str.replace(/[^aeiou]/gi, '').length;
+// }
